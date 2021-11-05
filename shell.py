80caaa7692e6cfaefa51ff84907576a08b3206e1
elie.kazan <elie.kazan@ens.uvsq.fr>
2021-11-05 10:16:17 +0100

    added some functions

--- shell.py
+++ shell.py
@@ -20,6 +20,57 @@ class MyShell(Cmd):
         for i in files:
             print(i)
 
+    def do_pwd(self,s):
+        print(os.getcwd())
+
+    def do_mkdir(self,s):
+        os.mkdir(s)
+    
+    def do_rmdir(self,s):
+        os.rmdir(s)
+    
+    def do_touch(self,s):
+        f = open(s,'w')
+        f.close()
+    
+    def do_cat(self,s):
+        f = open(s,'r')
+        print(f.read())
+        f.close()
+    
+    def do_cp(self,s):
+        print("copying " , s)
+        os.system("cp " + s)
+    
+    def do_mv(self,s):
+        print("moving " , s)
+        os.system("mv " + s)
+    
+    def do_chmod(self,s):
+        print("changing mode of " , s)
+        os.system("chmod " + s)
+    
+    def do_chown(self,s):
+        print("changing owner of " , s)
+        os.system("chown " + s)
+    
+    def do_chgrp(self,s):
+        print("changing group of " , s)
+        os.system("chgrp " + s)
+    
+    def do_find(self,s):
+        print("finding " , s)
+        os.system("find " + s)
+    
+    def do_grep(self,s):
+        print("grep " , s)
+        os.system("grep " + s)
+    
+    def do_sed(self,s):
+        print("sed " , s)
+        os.system("sed " + s)
+    
+    
 
 
 
